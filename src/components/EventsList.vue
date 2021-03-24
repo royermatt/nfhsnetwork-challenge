4259a46d9bab90079f8986cb0101870d93f8dfa1
Matt Royer <royer.matt@gmail.com>
2021-03-23 20:04:45 -0400

    Created project and initialized files

---
+++ src/components/EventsList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="events">
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue';
+    import moment from 'moment';
+    
+    Vue.prototype.moment = moment;
+
+    export default {
+        name: 'EventsList',
+        data (){
+            return {
+                events: []
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    #events {
+        color: #333333;
+    }
+</style>
